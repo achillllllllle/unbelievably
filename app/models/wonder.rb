9c6a44ed5c4517d0f20084d82b7d490ceaf3326b
Sacha Gigoi <gigoi.sacha@gmail.com>
2024-02-26 22:40:33 +0100

    fix model and extensive seed

--- app/models/wonder.rb
+++ app/models/wonder.rb
@@ -2,5 +2,5 @@ class Wonder < ApplicationRecord
   has_many :reservations, dependent: :destroy
   belongs_to :user
 
-  validates :title, :price_per_day, :location, :type, presence: true
+  validates :title, :price_per_day, :location, :category, presence: true
 end
