c2f4186ed30c68c44758d6f31bf1aec6eaa51921
AntoineBarthelemy <antoinebarthelemy60@gmail.com>
2024-02-26 16:17:12 +0100

    improving routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Declaration of routes
-  
-  root to: "wonders#index"
 
-  get "/wonders/:id", to: "wonders/show"
-  get "/wonders/:id", to: "reservations/new"
-  get "/wonders/new", to: "wonders/new"
-  post "/wonders/:id", to: "reservations/create"
-  post "/wonders/:id", to: "wonders/create"
+  devise_for :users
+  resources :wonders
 
-  get "/:username", to: "reservations/index"
-  patch "/:username", to: "reservations/update"
-  delete "/:username", to: "reservations/destroy"
+  # root to: "wonders#index"
+  # get "/wonders/:id", to: "wonders#show"
+  # get "/wonders/:id", to: "reservations#new"
+  # get "/wonders/new", to: "wonders#new"
+  # post "/wonders/:id", to: "reservations#create"
+  # post "/wonders/:id", to: "wonders#create"
 
 end
