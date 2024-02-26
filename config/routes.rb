442e479408009fda8a8103095805f6db43e7972d
Sacha Gigoi <gigoi.sacha@gmail.com>
2024-02-26 16:39:25 +0100

    fix rapide

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
   get "up" => "rails/health#show", as: :rails_health_check
-
+  root to: "wonders#index"
   # Declaration of routes
   devise_for :users
-  resources :wonders
+  resources :wonders do
+    resources :reservations
+  end
 end
