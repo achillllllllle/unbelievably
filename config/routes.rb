ab43d0ad352b1d327cffdbb326a2f85472c2f815
constance lesaffre <constance.lesaffre@hotmail.fr>
2024-02-26 16:49:54 +0100

    wonder-view-show

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,17 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Declaration of routes
-  
+
   root to: "wonders#index"
 
-  get "/wonders/:id", to: "wonders/show"
-  get "/wonders/:id", to: "reservations/new"
-  get "/wonders/new", to: "wonders/new"
-  post "/wonders/:id", to: "reservations/create"
-  post "/wonders/:id", to: "wonders/create"
+  get "/wonders/:id", to: "wonders#show"
+  get "/wonders/:id", to: "reservations#new"
+  get "/wonders/new", to: "wonders#new"
+  post "/wonders/:id", to: "reservations#create"
+  post "/wonders/:id", to: "wonders#create"
 
-  get "/:username", to: "reservations/index"
-  patch "/:username", to: "reservations/update"
-  delete "/:username", to: "reservations/destroy"
+  get "/:username", to: "reservations#index"
+  patch "/:username", to: "reservations#update"
+  delete "/:username", to: "reservations#destroy"
 
 end
