6629570833332aaa0638d7d24c0b66af1f974ae0
Sacha Gigoi <gigoi.sacha@gmail.com>
2024-02-27 12:15:47 +0100

    finished - clean

--- app/controllers/wonders_controller.rb
+++ app/controllers/wonders_controller.rb
@@ -1,4 +1,6 @@
 class WondersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     @wonders = Wonder.all
   end
@@ -15,19 +17,20 @@ class WondersController < ApplicationController
     end
   end
 
-
   def create
     @wonder = Wonder.new(wonder_params)
+    @wonder.user = current_user
     if @wonder.save
       redirect_to wonder_path(@wonder)
     else
       render :new, status: :unprocessable_entity
+      raise
     end
   end
 
   private
 
   def wonder_params
-    params.require(:wonder).permit(:title, :price_per_day, :type, :location)
+    params.require(:wonder).permit(:title, :price_per_day, :type, :location, :category, :content, :user_id)
   end
 end
