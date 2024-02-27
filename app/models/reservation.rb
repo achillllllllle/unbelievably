652211e86192494c17263822c87c53c67b793dcc
Oscar Fonty <oscar77.fonty@gmail.com>
2024-02-27 14:37:16 +0100

    seed et default value de accepted

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,5 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :wonder
 
-  validates :start_date, :end_date, :price, :accepted, presence: true
+  validates :start_date, :end_date, :price, presence: true
 end
