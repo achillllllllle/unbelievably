ae7398744ace0f3422de4d0e770ce36b642a7ff4
Oscar Fonty <oscar77.fonty@gmail.com>
2024-02-28 11:24:47 +0100

    book_date et nb_participants validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,5 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :wonder
 
-  validates :start_date, :end_date, :price, presence: true
-  validates :end_date, comparison: { greater_than: :start_date }
+  validates :book_date, :nb_participants, :price, presence: true
 end
