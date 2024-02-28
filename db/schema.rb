13d29f253905403d640fca8e686047ec6f4f03d0
Constance <constance.lesaffre@hotmail.fr>
2024-02-28 15:27:53 +0100

    Fix merge master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_28_104629) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_28_101827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
