49fad469da1b2b608f7591b44f905ad448163421
Constance <constance.lesaffre@hotmail.fr>
2024-02-29 11:14:56 +0100

    resolve pb reservations link

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -1,10 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["tabContent"];
+  static targets = ["tabContent", "tabLink"];
 
   connect() {
     console.log("Helloooooooooo")
+    this.showTab("wonders");
   }
 
   toggle(event) {
@@ -15,16 +16,32 @@ export default class extends Controller {
     this.tabContentTargets.forEach((tabContent) => {
       tabContent.style.display = "none";
     });
+    this.showTab(targetTabId);
+
+    const previousActiveTab = this.element.querySelector(".tab-underlined.active");
+      if (previousActiveTab) {
+        previousActiveTab.classList.remove("active");
+      }
+      event.target.classList.add("active");
 
     if (targetTabId === "wonders") {
       document.getElementById("reservations").classList.add("d-none")
+      document.getElementById("saved").classList.add("d-none")
       document.getElementById("wonders").classList.remove("d-none")
 
-    } else {
+    } else if (targetTabId === "reservations") {
       document.getElementById("reservations").classList.remove("d-none")
+      document.getElementById("saved").classList.add("d-none")
+      document.getElementById("wonders").classList.add("d-none")
+
+    } else if (targetTabId === "saved") {
+      document.getElementById("reservations").classList.add("d-none")
+      document.getElementById("saved").classList.remove("d-none")
       document.getElementById("wonders").classList.add("d-none")
     }
+  }
 
-    document.getElementById(targetTabId).style.display = "block";
+  showTab(tabId) {
+    document.getElementById(tabId).style.display = "block";
   }
 }
