02bb9a6e29ecd2a9cce8aa68d0297ba25e781b27
AntoineBarthelemy <antoinebarthelemy60@gmail.com>
2024-02-29 12:07:40 +0100

    Enhance input

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -11,6 +11,7 @@ class ReservationsController < ApplicationController
     @price = @quantity * @price_per_participant
     @reservation.price = @price
     if @reservation.save
+      raise
       redirect_to user_path(current_user)
     else
       redirect_to wonder_path(@wonder), alert: "Can't create reservation, check informations and try again"
