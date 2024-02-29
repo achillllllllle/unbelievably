b39814859dd3db17398810caa14195ce73b3ac67
Oscar Fonty <oscar77.fonty@gmail.com>
2024-02-29 16:38:53 +0100

    truc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_02_29_110006) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +80,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_29_110006) do
     t.bigint "user_id"
     t.text "content"
     t.text "long_description"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_wonders_on_user_id"
   end
 
